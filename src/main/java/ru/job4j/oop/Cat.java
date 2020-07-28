d15942236c9fd64a7d3bfd28fb49e2363cfec17f
macbook <79824177721@yandex.ru>
2020-07-28 13:57:23 +0500

    1.4. Вызов метода с возвращаемым типом. [#320244]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,8 +1,13 @@
 package ru.job4j.oop;
 
 public class Cat {
+    public String sound() {
+        String voice = "may-may";
+        return voice;
+    }
     public static void main(String[] args) {
         Cat perry = new Cat();
         Cat sparky = new Cat();
+        System.out.println(perry.sound());
     }
 }
