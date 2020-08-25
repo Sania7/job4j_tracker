541695bf77b67bf4bff84541989df9c528da1929
macbook <79824177721@yandex.ru>
2020-08-25 19:08:55 +0500

    5.1. Тестирование. Подготовка данных. [#320213]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -20,9 +20,19 @@ public class StartUITest {
         Tracker tracker =  new Tracker();
         Item item = new Item();
         tracker.add(item);
-        String[] answers = {item.getId(), "replaced item"};
+        String[] answers = {String.valueOf(item.getId()), "replaced item"};
         StartUI.replaceItem(new StubInput(answers), tracker);
         Item replaced = tracker.findById(item.getId());
         assertThat(replaced.getName(), is("replaced item"));
+    }
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item();
+        tracker.add(item);
+        String[] answers = {String.valueOf(item.getId()), "deleted item"};
+        StartUI.deleteItem(new StubInput(answers), tracker);
+        Item deleted = tracker.findById(item.getId());
+        assertThat(deleted.getName(), is("deleted item"));
     }
 }
