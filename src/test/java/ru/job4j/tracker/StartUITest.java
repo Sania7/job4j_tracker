946fb5ac630c0b58a2f7eb5cce2280b6708a8aa6
macbook <79824177721@yandex.ru>
2020-10-24 10:38:00 +0500

    1.1. Тесты на StartUI. [#320229]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -97,4 +97,19 @@ public class StartUITest {
         assertThat(out.toString(), is("Menu." + System.lineSeparator() + "0. Exit" + System.lineSeparator()
                                                             + "=== Exit ===" + System.lineSeparator()));
     }
+
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(new String[] {"1", "0"});
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {new Exit(out)};
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(String.format("Menu.%n"
+                + "0. Exit%n"
+                + "Wrong input, you can select: 0 .. 0%n"
+                + "Menu.%n"
+                + "0. Exit%n"
+                + "=== Exit ===%n")));
+    }
 }
