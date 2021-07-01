f9025f39206b854c5a483d752c952dc6a7635091
macbook <79824177721@yandex.ru>
2021-07-01 13:38:06 +0500

    2.2. Лямбда блок [#249208]

--- src/main/java/ru/job4j/lambda/LambdaUsage.java
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -1,20 +1,22 @@
 package ru.job4j.lambda;
 
+import java.util.Arrays;
 import java.util.Comparator;
 
 public class LambdaUsage {
 
     public static void main(String[] args) {
-
-        Comparator<String> cmpText = (left, right) -> {
-            //с выводом отладочной информации на консоль
-            System.out.println("compare - " + left + " : " + right);
-            return left.compareTo(right);
+        Attachment[] atts = {
+                new Attachment("image 1", 20),
+                new Attachment("image 3", 120),
+                new Attachment("image 2", 23),
         };
 
-        Comparator<String> cmpDescSize = (left, right) -> {
-            System.out.println("compare - " + right.length() + ":" + left.length());
-            return right.length() - left.length();
+        Comparator<Attachment> cmpDescSize = (left, right) -> {
+            System.out.println("compare - " + right.getSize() + ":" + left.getSize());
+            return Integer.compare(right.getSize(), left.getSize());
         };
+        Arrays.sort(atts, cmpDescSize);
+        System.out.println(Arrays.toString(atts));
     }
 }
