9ff26854b6cf8bebdc082e90738600f2bc83034a
macbook <79824177721@yandex.ru>
2021-07-01 11:35:31 +0500

    2.2. Лямбда блок [#249208]

---
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -0,0 +1,19 @@
+package ru.job4j.lambda;
+
+import java.util.Comparator;
+
+
+public class LambdaUsage {
+    public static void main(String[] args) {
+        Comparator<String> cmpText = (left, right) -> {
+            //с выводом отладочной информации на консоль
+            System.out.println("compare - " + left + " : " + right);
+            return left.compareTo(right);
+        };
+
+        Comparator<String> cmpDescSize = (left, right) -> {
+            System.out.println("compare - " + right.length() + ":" + left.length());
+            return right.length() - left.length();
+        };
+    }
+}
---
+++ src/main/java/ru/job4j/lambda/StringFunc.java
@@ -0,0 +1,29 @@
+package ru.job4j.lambda;
+
+import java.util.Locale;
+
+public interface StringFunc {
+    String func(String n);
+}
+
+
+class BlockLambdaDemo2 {
+    public static void main(String[] args) {
+        /**
+         * Блочное выражение, изменяющее на обратный порядок следования
+         * символов в строке.
+         */
+        StringFunc reverse = (str) -> {
+            String result = " ";
+            int i;
+            for (i = str.length()- 1; i >= 0; i--) {
+                result += str.toUpperCase(Locale.ROOT).charAt(i);
+            }
+            return result;
+        };
+        System.out.println("Лямбда обращается на "
+                           + reverse.func("Лямбда"));
+        System.out.println("Выражение обращается на "
+                + reverse.func("Выражение"));
+    }
+}
