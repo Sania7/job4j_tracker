beea3f37be1be691e692c8192360eddb5ac8f21e
macbook <79824177721@yandex.ru>
2021-07-02 20:24:25 +0500

    4. Зона видимости в лямбда-выражениях и исключения [#70609 #173545]

--- src/main/java/ru/job4j/lambda/VolumeInside.java
+++ src/main/java/ru/job4j/lambda/VolumeInside.java
@@ -4,10 +4,10 @@ import java.util.function.Supplier;
 
 public class VolumeInside {
     public static void main(String[] args) {
-        int[] number = {1, 2, 3};
+        int[] number = {4, 5, 6, 7, 8};
         int total = 0;
         for (int i : number) {
-            int num = number[i - 1];
+            int num = number[i];
             int tmp = total;
             total = add(() -> tmp + num);
         }
