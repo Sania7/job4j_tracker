705debb54b7073c0b5951022f718c98e05246193
macbook <79824177721@yandex.ru>
2021-07-02 10:55:02 +0500

    3. Ссылки на методы [#70608]

--- src/main/java/ru/job4j/lambda/RefMethodLambda.java
+++ src/main/java/ru/job4j/lambda/RefMethodLambda.java
@@ -12,27 +12,8 @@ public class RefMethodLambda {
                 "Dima Petuhov",
                 "Petr Arsentev"
         );
+
        // Consumer<String> out = (name) -> cutOut(name);
-        /**
-         * Ссылки бывают четырех видов
-         *
-         * Ссылка на статический метод
-         * ContainingClass::staticMethodName
-         *
-         * Ссылка на нестатический метод конкретного объекта
-         * containingObject::instanceMethodName
-         *
-         * Ссылка на нестатический метод любого объекта конкретного типа
-         * ContainingType::methodName
-         *
-         * Ссылка на конструктор
-         * ClassName::new
-         *
-         * names переменная в которой помещен список
-         * forEach цикл
-         * RefMethodLambd класс
-         * cutOut метод
-         */
         names.forEach(RefMethodLambda::cutOut);
 
     }
