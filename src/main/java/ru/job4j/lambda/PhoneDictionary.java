0234d80183ebe62d089a65ad5b6e84dd3a9007c6
macbook <79824177721@yandex.ru>
2021-07-03 16:05:36 +0500

    6. Функции высшего порядка [#24255]

--- src/main/java/ru/job4j/lambda/PhoneDictionary.java
+++ src/main/java/ru/job4j/lambda/PhoneDictionary.java
@@ -1,24 +1,29 @@
 package ru.job4j.lambda;
 
 import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
 import java.util.function.Predicate;
 
 public class PhoneDictionary {
+
     private ArrayList<Person> persons = new ArrayList<>();
 
     public void add(Person person) {
         this.persons.add(person);
     }
-//    public ArrayList<Person> find(String key) {
-//        Predicate<Person> predicate =
-//        ArrayList<Person> result = new ArrayList<>();
-//        for (Person p : persons) {
-//            if (combine.test(per)) {
-//
-//            }
-//        }
-//        return result;
-//    }
+
+    public ArrayList<Person> find(String key) {
+        Predicate<Person> filterByName = (s) -> s.getName().contains(key);
+        Predicate<Person> filterBySurname = (s) -> s.getSurname().contains(key);
+        Predicate<Person> filterByPhone = (s) -> s.getPhone().contains(key);
+        Predicate<Person> filterByAddres = (s) -> s.getAddress().contains(key);
+        Predicate<Person> combine = filterByName.or(filterBySurname)
+                .or(filterByPhone).or(filterByAddres);
+        ArrayList<Person> result = new ArrayList<>();
+        for (Person person : persons) {
+            if (combine.test(person)) {
+                result.add(person);
+            }
+        }
+        return result;
+    }
 }
