922ccfe56b87bad4f9b5d82a7d8e0d8705b6e2c1
macbook <79824177721@yandex.ru>
2021-07-04 07:48:02 +0500

    6. Функции высшего порядка [#24255]

--- src/test/java/ru/job4j/lambda/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/lambda/PhoneDictionaryTest.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
 
 public class PhoneDictionaryTest {
     @Test
@@ -16,5 +17,21 @@ public class PhoneDictionaryTest {
         );
         ArrayList<Person> persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
+    @Test
+    public void whenFidByAddress() {
+        PhoneDictionary address = new PhoneDictionary();
+        address.add(
+                new Person("Alexandr", "Bannikov", "477721", "Surgut")
+        );
+        ArrayList<Person> persons = address.find("Surgut");
+        assertThat(persons.get(0).getAddress(), is("Surgut"));
+    }
+    @Test
+    public void whenFindByNull() {
+        PhoneDictionary thereIsNothing = new PhoneDictionary();
+        thereIsNothing.add(new Person("", "", "", ""));
+        ArrayList<Person> persons = thereIsNothing.find(" ");
+        assertTrue( persons.isEmpty());
     }
 }
