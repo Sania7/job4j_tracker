1ae1f3b86a7f8513d1acc8654cc2328cd95b05e9
macbook <79824177721@yandex.ru>
2021-07-06 15:07:25 +0500

    2. Список адресов [#110062 #174647]

--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -2,6 +2,11 @@ package ru.job4j.stream;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 public class ProfilesTest {
     @Test
     public void address() {
@@ -10,5 +15,19 @@ public class ProfilesTest {
         Address addressThree = new Address("Novosibirsk", "Frunze", 112, 122);
         Address addressFour = new Address("Omsk", "Tverskaja", 45, 98);
         Address addressFive = new Address("Tumen", "Burovaja", 21, 115);
+
+        Profile profileOne = new Profile(addressOne);
+        Profile profileTwo = new Profile(addressTwo);
+        Profile profileThree = new Profile(addressThree);
+        Profile profileFour = new Profile(addressFour);
+        Profile profileFive = new Profile(addressFive);
+
+        List<Profile> profiles =
+                Arrays.asList(profileOne, profileTwo, profileThree, profileFour, profileFive);
+        List<Address> rsl = Profiles.collect(profiles);
+        List<Address> expected =
+                Arrays.asList(addressOne, addressTwo, addressThree, addressFour, addressFive);
+        assertEquals(rsl, expected);
+
     }
 }
