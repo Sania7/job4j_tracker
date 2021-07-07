b74359bc6a3201b2a75763f372a1cf56ae79e7c1
macbook <79824177721@yandex.ru>
2021-07-07 12:04:59 +0500

    3. Уникальность элементов и сортировка. [#110225]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -1,5 +1,6 @@
 package ru.job4j.stream;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,7 +17,7 @@ public class Profiles {
    public static List<Address> sort(List<Profile> profile) {
        return profile.stream()
                .map(Profile::getAddress)
-               .sorted()
+               .sorted(Comparator.comparing(Address::getCity))
                .distinct()
                .collect(Collectors.toList());
 
--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -27,9 +27,35 @@ public class ProfilesTest {
         List<Profile> profiles =
                 Arrays.asList(profileOne, profileTwo, profileThree, profileFour, profileFive);
         List<Address> rsl = Profiles.collect(profiles);
+
         List<Address> expected =
                 Arrays.asList(addressOne, addressTwo, addressThree, addressFour, addressFive);
         assertEquals(rsl, expected);
-
     }
+        @Test
+        public void whenSort() {
+            Address addressOne = new Address("Surgrt", "Krilova", 27, 233);
+            Address addressTwo = new Address("Tomsk", "Uchebnaja", 54, 3);
+            Address addressThree = new Address("Novosibirsk", "Frunze", 112, 122);
+            Address addressThree1 = new Address("Novosibirsk", "Frunze", 112, 122);
+            Address addressFour = new Address("Omsk", "Tverskaja", 45, 98);
+            Address addressFive = new Address("Tumen", "Burovaja", 21, 115);
+            Address addressFive1 = new Address("Tumen", "Burovaja", 21, 115);
+
+            Profile profileOne = new Profile(addressOne);
+            Profile profileTwo = new Profile(addressTwo);
+            Profile profileThree = new Profile(addressThree);
+            Profile profileThree1 = new Profile(addressThree1);
+            Profile profileFour = new Profile(addressFour);
+            Profile profileFive = new Profile(addressFive);
+            Profile profileFive1 = new Profile(addressFive1);
+
+            List<Profile> prof =
+                    Arrays.asList
+                            (profileOne, profileTwo, profileThree, profileThree1, profileFour, profileFive, profileFive1);
+            List<Address> sort = Profiles.sort(prof);
+            List<Address> expect = Arrays.asList(addressOne, addressTwo, addressThree, addressFour, addressFive);
+
+            assertEquals(sort, expect);
+        }
 }
