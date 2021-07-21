c2dee368d950fc9dd21cab665bb91cfeb01cc06d
macbook <79824177721@yandex.ru>
2021-07-21 10:38:07 +0700

    1. Метод .of [#70623]

--- src/test/java/ru/job4j/collection/NotifyAccountTest.java
+++ src/test/java/ru/job4j/collection/NotifyAccountTest.java
@@ -1,21 +1,21 @@
 package ru.job4j.collection;
 
 import org.junit.Test;
-import static org.hamcrest.core.Is.is;
-import java.util.Arrays;
+
 import java.util.HashSet;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class NotifyAccountTest {
     @Test
     public void sent() {
-        List<Account> accounts = Arrays.asList(
+        List<Account> accounts = List.of(
                 new Account("123", "Petr Arsentev", "eDer3432f"),
                 new Account("142", "Petr Arsentev", "000001"));
         HashSet<Account> expect = new HashSet<>(
-                Arrays.asList(
+                List.of(
                         new Account("123", "Petr Arsentev", "eDer3432f"),
                         new Account("142", "Petr Arsentev", "000001")
                 )
